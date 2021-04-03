778e19cefe4a1cd91e627ac88280663330c23f31
Codeliner <codeliner96@gmail.com>
2021-04-03 12:45:13 +0900

    Updated Router links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nesciunt
+      dicta nisi neque! Aspernatur ipsa dicta doloremque sapiente nam impedit
+      odio et similique illo recusandae fugit in molestiae, quaerat quibusdam
+      perspiciatis voluptate est itaque reiciendis quod, aliquid commodi nisi
+      suscipit! Provident quasi aperiam obcaecati ad nemo. Harum eligendi magni
+      cum est rem distinctio repellendus cupiditate sunt eaque laboriosam? Nihil
+      minus reiciendis itaque accusamus beatae ratione omnis amet non, quaerat
+      perspiciatis culpa deserunt nemo aperiam sequi consectetur neque tempora
+      autem. Distinctio adipisci consequatur nulla quasi quaerat, pariatur
+      maxime, fugit eius animi fuga aliquid. Numquam blanditiis voluptatem quasi
+      officiis, voluptatibus laborum enim?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse nesciunt
+      dicta nisi neque! Aspernatur ipsa dicta doloremque sapiente nam impedit
+      odio et similique illo recusandae fugit in molestiae, quaerat quibusdam
+      perspiciatis voluptate est itaque reiciendis quod, aliquid commodi nisi
+      suscipit! Provident quasi aperiam obcaecati ad nemo. Harum eligendi magni
+      cum est rem distinctio repellendus cupiditate sunt eaque laboriosam? Nihil
+      minus reiciendis itaque accusamus beatae ratione omnis amet non, quaerat
+      perspiciatis culpa deserunt nemo aperiam sequi consectetur neque tempora
+      autem. Distinctio adipisci consequatur nulla quasi quaerat, pariatur
+      maxime, fugit eius animi fuga aliquid. Numquam blanditiis voluptatem quasi
+      officiis, voluptatibus laborum enim?
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. At atque quae
+      quaerat veniam eos voluptate inventore tenetur. Excepturi, laudantium
+      maxime non hic impedit eveniet vitae aliquam corporis minus libero magnam
+      dolorum beatae! Debitis, doloremque voluptatibus saepe, temporibus sint
+      perspiciatis, tempora dolorum aut a aperiam laborum incidunt voluptate
+      velit sunt! Fugiat natus ut assumenda atque ab dolorem provident aliquid?
+      Dignissimos omnis vero doloribus corporis veritatis, architecto dicta ab!
+      Animi odit veritatis ut repellendus distinctio asperiores in soluta
+      facilis non. Animi ipsum perferendis nemo id vitae sunt voluptas saepe
+      illo, vero tenetur porro laborum, tempora dolorum modi dolores itaque ut
+      fuga nulla.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. At atque quae
+      quaerat veniam eos voluptate inventore tenetur. Excepturi, laudantium
+      maxime non hic impedit eveniet vitae aliquam corporis minus libero magnam
+      dolorum beatae! Debitis, doloremque voluptatibus saepe, temporibus sint
+      perspiciatis, tempora dolorum aut a aperiam laborum incidunt voluptate
+      velit sunt! Fugiat natus ut assumenda atque ab dolorem provident aliquid?
+      Dignissimos omnis vero doloribus corporis veritatis, architecto dicta ab!
+      Animi odit veritatis ut repellendus distinctio asperiores in soluta
+      facilis non. Animi ipsum perferendis nemo id vitae sunt voluptas saepe
+      illo, vero tenetur porro laborum, tempora dolorum modi dolores itaque ut
+      fuga nulla.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
